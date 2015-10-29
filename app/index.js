bac4c78aa19112e1c9279973e0b8029ccf3dd641
Ram <quadrupleslap@gmail.com>
2015-10-29 22:34:09 +1100

    Notices workign again?

--- app/index.js
+++ app/index.js
@@ -1,5 +1,6 @@
+import ReactDOM from 'react-dom';
 import React from 'react';
 
 import App from './components/app';
 
-React.render(<App />, document.body);
+ReactDOM.render(<App />, document.getElementById('app'));
