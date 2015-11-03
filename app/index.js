938677d1aa0a01b7e2dd4dc3ad0503ef043ff99e
Ram <quadrupleslap@gmail.com>
2015-11-03 21:27:28 +1100

    PLEASE LET THIS WORK FOR THE LOVE OF ALL THAT IS GOOD OR EVEN DECENT

--- app/index.js
+++ app/index.js
@@ -4,16 +4,3 @@ import React from 'react';
 import App from './components/app';
 
 ReactDOM.render(<App />, document.getElementById('app'));
-
-//TODO: Put this somewhere better. And fix overflow scrolling if this doesn't do it.
-let elem = document.body;
-elem.addEventListener('touchstart', e => {
-  startY = event.touches[0].pageY;
-  startTopScroll = elem.scrollTop;
-
-  if(startTopScroll <= 0)
-    elem.scrollTop = 1;
-
-  if(startTopScroll + elem.offsetHeight >= elem.scrollHeight)
-    elem.scrollTop = elem.scrollHeight - elem.offsetHeight - 1;
-});
