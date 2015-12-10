1e140189685da93a5589ad01fd547e93236df3eb
Ram <quadrupleslap@gmail.com>
2015-12-11 03:42:40 +1100

    Well, now it kinda' works.

--- easter-egg.js
+++ easter-egg.js
@@ -1,13 +1,17 @@
 var ytdl = require('ytdl-core');
 
 var YOUTUBE_OPTIONS = {
-  filter: 'video',
+  filter: function (format) {
+    return format.container === 'mp4' && format.audioBitrate && format.resolution;
+  },
   quality: 'lowest'
 };
 
 module.exports = function (app) {
+  //TODO: Use 3gp if iPhone.
   app.get('/e/stream/:id', function (req, res) {
-    ytdl('http://www.youtube.com/watch?v=' + req.params.id, YOUTUBE_OPTIONS)
-      .pipe(res);
+    var video = ytdl('http://www.youtube.com/watch?v=' + req.params.id, YOUTUBE_OPTIONS);
+    res.type('video/mp4');
+    video.pipe(res);
   });
 }
