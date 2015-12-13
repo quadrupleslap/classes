e795bff5f207bed83123bcc458f652b602d06b78
Ram <quadrupleslap@gmail.com>
2015-12-14 02:01:55 +1100

    Toggle button!

--- app/components/settings.js
+++ app/components/settings.js
@@ -1,8 +1,18 @@
 import React from 'react';
+
 import Centered from './centered';
+import Toggle from './toggle';
 
 export default React.createClass({
+  getInitialState() {
+    return { enabled: false };
+  },
+
   render() {
-    return <Centered vertical horizontal>Coming soon (like in the next few days)!</Centered>;
+    return <Centered vertical horizontal>
+      <p>Coming soon (like in the next few days)!</p>
+      <p>Meanwhile, you can click this toggle button (which took ages to make):</p>
+      <Toggle enabled={this.state.enabled} onChange={(newState) => this.setState({ enabled: newState })} />
+    </Centered>;
   }
 });
